7638cbb36fb148576d62ac7de32b0f92956ab82b
Alande Amorim <alande.amorim@gmail.com>
2020-02-13 19:00:22 -0300

    initial commit

---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import {Title } from './styles';
+
+class Login extends Component {
+  render() {
+    return <Title>Login</Title>;
+  }
+}
+
+export default Login;
